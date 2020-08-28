d89120a27b5248d6f4ac86829294f1e2c76d2694
atmirala <atmirala@gmail.com>
2020-08-28 00:55:47 -0300

    conflitos nos posts resolvido

--- src/data/PostDatabase.ts
+++ src/data/PostDatabase.ts
@@ -6,6 +6,27 @@ export default class PostsDatabase extends BaseDatabase {
   private static TABLE_NAME = "labook_post";
   private static LIMIT = 5;
 
+  public async createPost(
+    postId: string,
+    urlPhoto: string,
+    description: string,
+    creationDate: string,
+    type: string,
+    userCreatorId: string
+  ): Promise<void> {
+    await this.getConnection()
+      .insert({
+        post_id: postId,
+        url_photo: urlPhoto,
+        description: description,
+        creation_date: creationDate,
+        type: type,
+        user_creator_id: userCreatorId,
+      })
+      .into(PostsDatabase.TABLE_NAME);
+    await BaseDatabase.destroyConnection();
+  }
+
   public async getFeedByUserId(
     userId: string,
     page: number,
